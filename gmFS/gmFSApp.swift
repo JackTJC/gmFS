62bb2920f3fc1df7391ced7f954c9aee9a9dad45
JincaiTian <tianjincai@hotmail.com>
2022-02-14 23:47:02 +0800

    Initial Commit

---
+++ gmFS/gmFSApp.swift
@@ -0,0 +1,17 @@
+//
+//  gmFSApp.swift
+//  gmFS
+//
+//  Created by jincaitian on 2022/2/14.
+//
+
+import SwiftUI
+
+@main
+struct gmFSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
