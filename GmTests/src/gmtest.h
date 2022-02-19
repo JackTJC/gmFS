65a3351ee04f48adb01fc668bb7677cc26ba9e0b
JincaiTian <tianjincai@hotmail.com>
2022-02-19 17:15:30 +0800

    update

--- GmTests/src/gmtest.h
+++ GmTests/src/gmtest.h
@@ -7,13 +7,9 @@
 
 #ifndef Header_h
 #define Header_h
-# include <openssl/engine.h>
-# include <string.h>
-
 int sm3test(void);
 int sm2test(void);
 int sm9test(void);
 int zuctest(void);
 int sms4test(void);
-
 #endif /* Header_h */
