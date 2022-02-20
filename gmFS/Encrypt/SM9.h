1a5b79a22c6ce8085c7d067fe0661505e07c4c01
tianjincai.jack <tianjincai.jack@bytedance.com>
2022-02-20 19:01:27 +0800

    update:add sm9

---
+++ gmFS/Encrypt/SM9.h
@@ -0,0 +1,16 @@
+//
+//  SM9.h
+//  gmFS
+//
+//  Created by bytedance on 2022/2/20.
+//
+
+#ifndef SM9_h
+#define SM9_h
+
+@interface SM9Encryption : NSObject
+
+
+@end
+
+#endif /* SM9_h */
---
+++ gmFS/Encrypt/SM9.m
@@ -0,0 +1,9 @@
+//
+//  SM9.m
+//  gmFS
+//
+//  Created by bytedance on 2022/2/20.
+//
+
+#import <Foundation/Foundation.h>
+#import "SM9.h"
