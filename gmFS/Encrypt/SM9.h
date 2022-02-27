2545fa8d41a30c32ad64fd210453bbc685e986d7
tianjincai.jack <tianjincai.jack@bytedance.com>
2022-02-27 17:45:40 +0800

    update: add some implement

--- gmFS/Encrypt/SM9.h
+++ gmFS/Encrypt/SM9.h
@@ -7,9 +7,17 @@
 
 #ifndef SM9_h
 #define SM9_h
+#include <openssl/sm9.h>
 
 @interface SM9Encryption : NSObject
+@property SM9PublicParameters *mpk;
+@property SM9MasterSecret *msk;
+@property SM9PrivateKey *sk;
+@property const char *identification;
 
+- (id)init:(NSString *)identification;
+- (NSData *)encrypt:(NSData *)data;
+- (NSData *)decrypt:(NSData *)data;
 
 @end
 
